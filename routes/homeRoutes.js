e308b8ca4d5875f8f64c87915bb5ce1c17cda29c
Ethan De La Rosa <dlrethan10@gmail.com>
2022-12-21 19:03:54 -0800

    did the basics to the routes and utils folder. Also added the package.json and package-lock.json files. Also added the .gitignore file.

---
+++ routes/homeRoutes.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ["password"] },
+      order: [["name", "ASC"]],
+    });
+    const users = userData.map((project) => project.get({ plain: true }));
+    res.render("homepage", {
+      users,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
+module.exports = router;
