b3ddcd450213d31c7538e66030f5d51e9e7625ba
Ethan De La Rosa <dlrethan10@gmail.com>
2022-12-22 20:50:35 -0800

    i honestly forgot what i did

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
-const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./controllers/connection');
+const express = require("express");
+const routes = require("./routes");
+const sequelize = require("./controllers/connection");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-sequelize.sync({ force : false }).then(() => {
-    app.listen(PORT, () => {
-        console.log(`App listening on port ${PORT}`);
-    })
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+  });
 });
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./public/src/input.css"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
