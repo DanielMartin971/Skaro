bc932badc4994891f3bf77e027734042f4eb4bab
TDGLU <tdglukealt@gmail.com>
2023-01-05 19:12:41 -0800

    fixed register and login buttons

--- public/js/login.js
+++ public/js/login.js
@@ -2,3 +2,7 @@
 const loginFormHandle = async (event) => {
     event.preventDefault();
 }
+
+// Grab user username and password
+const email = document.getElementById("emailInput").value;
+const password = document.getElementById("passwordInput").value;
