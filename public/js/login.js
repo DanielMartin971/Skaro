6e34eaa78ba4e88c80020bbf1f6ac8de60b14cfa
TDGLU <tdglukealt@gmail.com>
2023-01-05 20:02:51 -0800

    fixed grabbing email and password input for login page

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,21 @@
-//Add and collect values from the login form
-const loginFormHandle = async (event) => {
-    event.preventDefault();
-}
+// //Add and collect values from the login form
+// const loginFormHandle = async (event) => {
+//     event.preventDefault();
+// }
 
-// Grab user username and password
-const email = document.getElementById("emailInput").value;
-const password = document.getElementById("passwordInput").value;
+const loginButton = document.querySelector('#loginButton')
+const loginForm = document.getElementById('loginForm')
+
+loginButton.addEventListener('click', function(event) {
+    event.preventDefault()
+    console.log('CLICKED')
+    // Grab user username and password
+    const email = document.getElementById("emailInput").value
+    const password = document.getElementById("passwordInput").value
+    
+    login(email, password)
+})
+
+loginButton.addEventListener('click', function() {
+    console.log('CLICKED')
+})
