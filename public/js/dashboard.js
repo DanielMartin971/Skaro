334ee0f45dd83ca973071d6a86eab0615c56f96e
Bailey Mejia <baileymejia28@gmail.com>
2023-01-05 21:18:33 -0800

    Added JavaScript to the modals and added a modal to the dashboard.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,11 +1,18 @@
-// TODO: ADD JAVASCRIPT FOR THE BUTTONS AND MUCH MORE
-
+//Mobile menu will display when this is used
 const mobileNav = document.getElementById('res-btn');
 const menuDisplay = document.getElementById('mobile-menu');
+
+//signout button will be accessed through profile icon
 const userMenu = document.getElementById('signout-btn');
 const signoutMenu = document.getElementById('signoutHide');
 
-//TODO Fix the bug that always shows the menu for mobile screens
+//TODO getElementById for modal access
+const modalDashboardTrigger = document.getElementById('modalDashboardTrigger');
+const dashboardModal = document.getElementById('dashboardModal');
+const closeDashboardTrigger = document.getElementById('closeDashTrigger');
+console.log(closeDashboardTrigger);
+
+//Open the mobile menu and close the mobile menu
 mobileNav.addEventListener('click', () => {
     if (menuDisplay.style.display === 'none') {
         menuDisplay.style.display = 'block';
@@ -14,11 +21,21 @@ mobileNav.addEventListener('click', () => {
     }
 });
 
+//Open sign out menu and close it
 userMenu.addEventListener('click', () => {
     if (signoutMenu.style.display === 'none') {
         signoutMenu.style.display = 'block';
     } else {
         signoutMenu.style.display = 'none';
     }
-})
+});
+
+//toggle the modal open to show input form
+modalDashboardTrigger.addEventListener('click', () => {
+    dashboardModal.classList.toggle('modal-open');
+});
+//Toggle the modal to close on click
+closeDashboardTrigger.addEventListener('click', () => {
+    dashboardModal.classList.remove('modal-open');
+});
 
