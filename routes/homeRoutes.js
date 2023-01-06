5cf3d065ffb03f88dad4a8c400cfa46a8022f140
Ethan De La Rosa <dlrethan10@gmail.com>
2023-01-05 21:15:35 -0800

    fixed some routes

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,16 +1,19 @@
 const router = require("express").Router();
 const { User } = require("../models");
+const Employee = require("../models/Employee");
 const withAuth = require("../utils/auth");
 
 router.get("/", withAuth, async (req, res) => {
   try {
-    const userData = await User.findAll({
+    const employeeData = await Employee.findAll({
       attributes: { exclude: ["password"] },
       order: [["name", "ASC"]],
     });
-    const users = userData.map((project) => project.get({ plain: true }));
+    const employees = Employee.Data.map((project) =>
+      project.get({ plain: true })
+    );
     res.render("homepage", {
-      users,
+      employees,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
