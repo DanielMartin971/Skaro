7475a9e9c52571806a2f9f4431af7cdb5ebf37bc
Daniel Martin <51744227+DanielMartin971@users.noreply.github.com>
2023-01-06 23:09:31 -0800

    added in comments to some of the files explaining more details

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,16 @@
-const loginButton = document.getElementById('signUpButton')
-const loginForm = document.getElementById('signUpForm')
+const loginButton = document.getElementById('signUpButton');
+const loginForm = document.getElementById('signUpForm');
 
 loginButton.addEventListener('click', function(event) {
-    event.preventDefault()
-    console.log('CLICKED')
+    event.preventDefault();
+    console.log('CLICKED');
     // Grab user username and password
-    const email = document.getElementById("emailInput").value
-    const password = document.getElementById("passwordInput").value
-    console.log(email, password)
-    login(email, password)
+    const email = document.getElementById("emailInput").value;
+    const password = document.getElementById("passwordInput").value;
+    console.log(email, password);
+    login(email, password);
 })
 
 loginButton.addEventListener('click', function() {
-    console.log('CLICKED')
+    console.log('CLICKED');
 })
