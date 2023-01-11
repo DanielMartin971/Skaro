bfd5061a86f2c908ed315e446cd2aca73bd029bb
Daniel Martin <51744227+DanielMartin971@users.noreply.github.com>
2023-01-10 22:17:48 -0800

    added in a part to the server to hopefully fix problems on heroku

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(routes);
 
