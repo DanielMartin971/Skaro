889f1207635eeb1d98f427dda5e994ba7db481b7
Bailey Mejia <baileymejia28@gmail.com>
2023-01-11 21:58:52 -0800

    Fixed a bunch of issues with the pages check the issues page in repo.

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -1,7 +1,13 @@
 const button = document.getElementById('res-btn');
 const menuDisplay = document.getElementById('mobile-menu');
-const taskMenu = document.getElementById('user-menu-button');
-const taskSignout = document.getElementById('taskSignout');
+//Code below is from dash.js
+const userMenu = document.getElementById('signout-btn');
+const signoutMenu = document.getElementById('signoutHide');
+
+//Leave taskMenu and tasksignout commented out DONT DELETE
+// const taskMenu = document.getElementById('user-menu-button');
+// const taskSignout = document.getElementById('taskSignout');
+
 
 //TODO Fix the bug that always shows the menu for mobile screens
 button.addEventListener('click', () => {
@@ -11,13 +17,22 @@ button.addEventListener('click', () => {
         menuDisplay.style.display = 'none';
     }
 });
-
-//Hides the signout uption on click
-taskMenu.addEventListener('click', () => {
-    if (taskSignout.style.display === 'none') {
-        taskSignout.style.display = 'block';
+//added the same id elements from dashboard.js to all pages
+userMenu.addEventListener('click', () => {
+    if (signoutMenu.style.display === 'none') {
+        signoutMenu.style.display = 'block';
     } else {
-        taskSignout.style.display = 'none';
+        signoutMenu.style.display = 'none';
     }
 });
+
+//DONT DELETE
+//Hides the signout uption on click
+// taskMenu.addEventListener('click', () => {
+//     if (taskSignout.style.display === 'none') {
+//         taskSignout.style.display = 'block';
+//     } else {
+//         taskSignout.style.display = 'none';
+//     }
+// });
 
